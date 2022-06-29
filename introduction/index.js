7f707be304db402889befdd5aaaeb3dfcee99fce
Marcelearn <jaworskimarcel@gmail.com>
2022-06-29 16:12:34 +0200

    erste commit

---
+++ introduction/index.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+let vorname = "Marcel"
+let land = "Venezuela"
+let essen = "Arepas"
+let essenZwei = "Pizza"
+let alter = "36"
+const geburstJahr = "1985"
+console. log(
+    "Hallo ich bin",vorname, "ich komme aus",land,"."
+    )
+console. log(
+    "Meine Lieblingsspeisen sind", essen, "und",essenZwei,"."
+    )
+console. log(
+    "Ich bin", alter,"Jahre alt und",geburstJahr,"geboren."
+)
---
+++ nodejs/index.js
@@ -0,0 +1,66 @@
+let vorname = "Marcel"
+let land = "Venezuela"
+let essen = "Arepas"
+let essenZwei = "Pizza"
+let alter = "36"
+const geburstJahr = "1985"
+console. log(
+    "Hallo ich bin",vorname, "ich komme aus",land,"."
+    )
+console. log(
+    "Meine Lieblingsspeisen sind", essen, "und",essenZwei,"."
+    )
+console. log(
+    "Ich bin", alter,"Jahre alt und",geburstJahr,"geboren."
+)
+//Löse die folgenden Aufgaben mit JavaScript und gib die Lösungen auf der Konsole aus.
+//Benutze sinnvolle Variablennamen.
+//A: “Ein Olympisches Schwimmbecken enthält (bei einer Wassertiefe von 3 m) 3 750 m3 Wasser. Wenn keine Wettkämpfe stattfinden wird aus Kostengründen 5 cm weniger eingelassen. Wie viele l Wasser sind das?”
+
+let wassertiefeZwei = 295
+let wassertiefe = 300
+let wasser = 3750
+console. log(
+    "A:",wassertiefe * wasser / wassertiefeZwei, "m3 Wasser"
+)
+//B: “Mein Auto hat einen Verbrauch von 8 l/100 km. Wie weit komme ich mit einer Tankfüllung (66 l)?”
+
+let gasGebrauch = 8
+let distanz = 100
+let tank = 66
+console. log(
+    "B:",tank / distanz * gasGebrauch, "Km"
+)
+//C: “3 Arbeiter teeren die neue Unterführung und brauchen dafür 17h. Wie lange würde es dauern, wenn 5 Arbeiter eingesetzt wären?”
+
+let arbeiter = 3
+let stunden = 17
+let mehrArbeiter = 5
+console. log(
+    "C:", arbeiter * stunden / mehrArbeiter, "h"
+)
+//D: “882 / 14 x 12 / 2.3 x 999"
+
+let achthunderdzweiundachtzig = 882
+let viersen = 14
+let zwolf = 12
+let zweipuntdrei = 2.3
+let neuenhunderdneuenundneuenzig = 999
+console. log(
+    "D:", achthunderdzweiundachtzig / (viersen * zwolf) / (zweipuntdrei * neuenhunderdneuenundneuenzig)
+)
+//E: “12 x 7 x 17 / 7 / 17 + 333”
+
+let sieben = 7
+let dreihunderdreiunddreizig = 333
+console. log(
+    "E:", zwolf * sieben * stunden/ sieben / stunden + dreihunderdreiunddreizig
+);
+//F: “Familie Bayerl will einen neuen Gartenzaun an der 16,80 m langen Grundstücksgrenze ziehen. Alle 120 cm muss ein Sockel betoniert werden. Pro Sockel muss die Familie 24,40 € bezahlen. Wie viel Geld braucht die Familie für die benötigten Sockel?”
+let grenzelange = 1680
+let sockeldistanz = 120
+let sockelkostet = 24.40
+console. log(
+    "F:", grenzelange / sockeldistanz * sockelkostet *100/100, "Euro"
+)
+
