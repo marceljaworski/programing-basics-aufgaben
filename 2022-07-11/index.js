7404e2ea64e95eb23c229d8272a2850957feaa60
Marcelearn <jaworskimarcel@gmail.com>
2022-07-12 14:12:12 +0200

    Aufgabe fertig

--- 2022-07-11/index.js
+++ 2022-07-11/index.js
@@ -27,20 +27,51 @@ switch (tag) {
 
 //** For Schleifen
 // Start   ;  check  ; danach
-for(let i =0; i <= 10; i++){
-
-    console.log(i);
-
-}
+//for(let i =0; i <= 10; i++){
+//    console.log(i);
+//}
 
 //* Break and continue
-for (let j = 0; j <= 100; j++){
-    console.log("break beispiel:", j);
-    if (i===5) break;
-}
+//for (let j = 0; j <= 100; j++){
+//    console.log("break beispiel:", j);
+//    if (i===5) break;
+//}
 
-for (let k = 0; k <= 10; k++){
-    console.log("break beispiel:", k);
-    if (k===5) continue;
-    
-}
+//for (let k = 0; k <= 10; k++){
+//    console.log("break beispiel:", k);
+//    if (k===5) continue;    
+//}
+
+//Teil 1:
+//Du bist für die Implementierung eines Warenkorb-Systems zuständig. Ziel ist es dem Nutzer des Onlineshops das teuerste Produkt, das günstigste Produkt und die Gesamtkosten für alle Produkte anzuzeigen.
+//Schreibe einen Algorithmus der für folgenden Input:
+//const cost = [2.39, 13.99, 4.19, 59.99, 1.99];
+//const productNames = ["Zahncreme","Tofu", "Müsli", "Toaster", "Kaugummi"];
+//Folgenden Output auf der Konsole anzeigt:
+//"Das teuerste Produkt ist: Toaster"
+//"Das billigste Produkt ist: Kaugummi"
+//"Alle 5 Produkte zusammen kosten: 82,55€"
+//Info: Runde alle Preise auf zwei Nachkommastellen.
+//Bonus:
+//Jetzt besteht der input aus drei Arrays. Zusätzlich zu den Preisen und den Produktnamen kommt jetzt noch ein Array amounts mit der Anzahl der jeweiligen Produkte.
+//const amounts = [4, 1, 2, 1, 3];
+//Die Zahncreme wird also 4 mal gekauft. Der Tofu 1 mal und so weiter.
+//Passe den Output von oben an sodass die Gesamtanzahl an Produkten und der Gesamtpreis stimmen. (bearbeitet) 
+
+const cost =  [2.39, 13.99, 4.19, 59.99, 1.99];
+const productNames = ["Zahncreme","Tofu", "Müsli", "Toaster", "Kaugummi"];
+const teuerste = Math.max(2.39, 13.99, 4.19, 59.99, 1.99);
+teuersteProdukt = productNames.slice (3, 4);
+console.log(`Das teuereste Produkt ist: ${teuersteProdukt} und costet ${teuerste}$`)
+const billigste = Math.min(2.39, 13.99, 4.19, 59.99, 1.99)
+billigsteProdukt = productNames.slice (4, 5);
+console.log(`Das billigste Produkt ist: ${billigsteProdukt} und costet ${billigste}$`);
+
+const zahnCreme = +cost.slice(0, 1).join();
+const tofu = +cost.slice(1, 2).join();
+const musli = +cost.slice(2, 3).join();
+const toaster = +cost.slice(3, 4).join();
+const kauGummi = +cost.slice(4, 5).join();
+
+const plusResult = zahnCreme + tofu + musli + toaster + kauGummi;
+console.log(`Alle 5 Produkte zusammen kosten: ${plusResult}$`);
---
+++ 2022-07-12/index.js
@@ -0,0 +1,77 @@
+// Wir haben die folgende Funktion erhalten, die wir für zwei Aufträge verwenden wollen.
+// Die Funktion selbst dürfen wir nicht verändern, aber beliebig oft ausführen.
+
+// Wichtig:
+// Unsere zwei Aufträge sollen mit beliebig vielen Zufallszahlen funktionieren.
+// Wir müssen daher darauf achten, dass unser Algorithmus mit unterschiedlichen Mengen arbeiten kann.
+
+function getRandomNumbers(amount) {
+    const numbers = Array();
+
+    for (let i = 0; i < amount; i++) {
+        numbers.push(parseInt(Math.random() * 100));
+    }
+
+    return numbers;
+}
+
+
+// Beispiel: gib 10 Zufallszahlen aus und speichere sie in der Variable randomNumbers
+const randomNumbers = getRandomNumbers(10);
+console.log(randomNumbers);
+
+// --------------------------------------------------------------------------
+//1.Losung
+const unter50 = randomNumbers.filter(element => element < 50); 
+console.log(` Es gibt ${unter50.length} Zahlen unter 50`);
+
+//2. Losung
+const unter50Array = unter50.slice();
+//console.log(unter50Array)
+//console.log(unter50)
+console.log(`Zahlen unter 50: `, unter50Array);
+
+const grosserGleich50 = randomNumbers.filter(element => element >= 50); 
+const grosserGleich50Array = grosserGleich50.slice();
+console.log(`Zahlen größer oder gleich 50: `,grosserGleich50Array);
+
+
+// 1. Gib die Anzahl der Zahlen unter 50 aus.
+// Gib zur Kontrolle auch das Array der Zufallszahlen aus.
+
+// Beispiel:
+// getRandomNumbers(5) => [1, 59, 40, 50, 99]
+// Ausgabe:
+// Es gibt 2 Zahlen unter 50.
+
+// Teste deinen Algorithmus mit den Anzahlen 10, 20, 100 und beliebig vielen weiteren.
+
+//--------------------------------------------------------------------------
+
+// 2. Gib zwei Arrays aus: Eines mit den Zahlen unter 50, und eines mit den Zahlen größer oder gleich 50.
+// Gib zur Kontrolle auch das Array der Zufallszahlen aus.
+
+// Beispiel:
+// getRandomNumbers(5) => [1, 59, 40, 50, 99]
+// Ausgabe:
+// Zahlen unter 50: [1, 40]
+// Zahlen größer oder gleich 50: [59, 50, 99]
+
+
+// Teste deinen Algorithmus mit den Anzahlen 10, 20, 100 und beliebig vielen weiteren.
+
+//Alternative Losung.. Ella`s Losung
+let counter = 0;
+let grosser50 = [];
+let kleiner50 = [];
+for (let i = 0; i < randomNumbers.length; i++){
+    if (randomNumbers[i] < 50){
+        counter++;
+        kleiner50.push(randomNumbers[i]);
+    } else {
+        grosser50.push(randomNumbers[i]);
+    }
+}
+//console.log(`Es gibt ${counter} Zahlen unter 50`);
+//console.log(` Zahlen unter 50: ${kleiner50}`);
+//console.log(`Zahlen grosser oder gleich 50: ${grosser50}`)
