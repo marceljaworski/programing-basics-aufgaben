63cf3257d36e87bf2106bc78c36c02e23eeeba56
Marcelearn <jaworskimarcel@gmail.com>
2022-08-31 16:00:11 +0200

    js exercises

--- 2022-08-31/index.js
+++ 2022-08-31/index.js
@@ -1,28 +1,4 @@
-// # Aufgabe 1
-// ​
-// [Englische Version](https://edabit.com/challenge/Gpy2qSFnfhGJnWMMj)
-// ​
-// Erstellen Sie eine Funktion, die true zurückgibt, wenn das erste Array im zweiten verschachtelt werden kann.
-// ​
-// arr1 kann innerhalb von arr2 verschachtelt werden, wenn:
-// ​
-// -   arr1's kleinste Zahl größer ist als arr2's kleinste Zahl.
-// -   arr1's größte Zahl kleiner ist als arr2's größte Zahl.
-// ​
-// ## Beispiele:
-// ​
-// ```js
-// ​
-// canNest([1, 2, 3, 4], [0, 6]) ➞ true
-// ​
-// canNest([3, 1], [4, 0]) ➞ true
-// ​
-// canNest([9, 9, 8], [8, 9]) ➞ false
-// ​
-// canNest([1, 2, 3, 4], [2, 3]) ➞ false
-// ​
-// ```
-
+console.log("# Aufgabe 1")
 // function canNest (a, b){
     
 //     if ( Math.min(...a) > Math.min(...b) && Math.max(...a) < Math.max(...b)){
@@ -37,3 +13,56 @@ console.log(canNest([1, 2, 3, 4], [0, 6]));
 console.log(canNest([3, 1], [4, 0]));
 console.log(canNest([9, 9, 8], [8, 9]));
 console.log(canNest([1, 2, 3, 4], [2, 3]));
+
+
+console.log("# Aufgabe 2")
+class Circle { 
+    constructor(radius){
+        this.radius = radius;
+    } 
+    getArea() { console.log( Math.PI * Math.pow(this.radius, 2)); }
+    getPerimeter() { 
+        console.log( (2 * Math.PI) * this.radius); }
+}
+let circy1 = new Circle(11);
+
+circy1.getArea(); // Should return 380.132711084365
+
+let circy4 = new Circle(4.44);
+circy4.getPerimeter(); // Should return 27.897342763877365
+
+console.log("# Aufgabe 3") 
+
+
+function makePlusFunction(zahl) {
+    return number = (number) => {
+        return number + zahl
+    }
+    
+}
+const plusFive = makePlusFunction(5);
+
+console.log(plusFive(2)); // returns 7
+console.log(plusFive(-8)); // returns -3
+
+const plusTen = makePlusFunction(10);
+
+console.log(plusTen(0)); // returns 10
+console.log(plusTen(188)); // returns 198
+
+
+console.log("# Aufgabe 4") 
+
+function colorPatternTimes(array) {
+    let arr = [array[0]];
+    for (let word of array) {
+      if (arr[arr.length - 1] !== word) arr.push(word);
+    }
+    return arr.length - 1 + array.length * 2;
+  }
+
+console.log(colorPatternTimes(["Blue"])) // return 2
+
+console.log(colorPatternTimes(["Red", "Yellow", "Green", "Blue"])) // return 11
+console.log(colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"])) // return 13
+console.log(colorPatternTimes(["Red", "Blue", "Red", "Blue", "Red"])) // return 14
